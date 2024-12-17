943ea0eb6f77ce73e94ec2d404b640cc448ba45d
fivegunners <fivegunners5@gmail.com>
2024-12-17 17:06:49 +0330

    Update settings.py

--- YekiKamePrj/settings.py
+++ YekiKamePrj/settings.py
@@ -156,7 +156,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = '/app/static_media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
 
 # Default primary key field type
