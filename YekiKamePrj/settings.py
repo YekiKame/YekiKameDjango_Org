5059effed635037b956fa25cc9b5df74d9b4b0d9
fivegunners <fivegunners5@gmail.com>
2024-12-17 21:26:31 +0330

    Update settings.py

--- YekiKamePrj/settings.py
+++ YekiKamePrj/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = '/app/static_media/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
