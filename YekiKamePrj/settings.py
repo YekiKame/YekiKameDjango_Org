eb677926954e9b037599a3d5df8be929e62caa19
fivegunners <fivegunners5@gmail.com>
2024-12-17 17:15:26 +0330

    Update settings.py

--- YekiKamePrj/settings.py
+++ YekiKamePrj/settings.py
@@ -155,10 +155,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = '/app/static_media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
+MEDIA_URL = 'media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
