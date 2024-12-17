4d779cccfbe95c2af35ca310ae6430169d15bc95
fivegunners <fivegunners5@gmail.com>
2024-12-17 20:29:39 +0330

    Update settings.py

--- YekiKamePrj/settings.py
+++ YekiKamePrj/settings.py
@@ -157,11 +157,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = '/app/static_media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
 # Serve static files during development
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 MEDIA_URL = 'media/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
 
